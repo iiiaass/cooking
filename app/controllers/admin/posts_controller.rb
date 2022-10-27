b76e2ee82a6488ab94c288fa348836593d73ce47
EC2 Default User <ec2-user@ip-172-31-7-78.us-west-1.compute.internal>
2022-10-27 14:42:06 +0000

    [Add]cooking

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,10 @@
 class Admin::PostsController < ApplicationController
+  
   def index
   end
 
   def show
   end
+  
+  
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,9 +1,17 @@
 class Public::PostsController < ApplicationController
   def new
+   @post=Post.new
+  end
+
+  def create
+   post=Post.new(post_params)
+   post.customer_id=current_customer.id
+   post.save!
+   redirect_to posts_path
   end
 
   def index
-
+   @posts=Post.all
   end
 
   def show
@@ -11,5 +19,10 @@ class Public::PostsController < ApplicationController
 
   def search
     @posts = Post.search(params[:search])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:article,:image,:genre_id)
   end
 end
